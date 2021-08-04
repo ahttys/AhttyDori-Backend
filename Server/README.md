6217fce1fa60cbecac9d01c28dcd28f36d8e8ad9
jrhong95 <jrhong95@gmail.com>
2021-08-04 21:31:21 +0900

    Add API test

---
+++ Server/README.md
@@ -0,0 +1,7 @@
+### Dependancy
+* OpenJDK 11.0.11
+* Springboot 2.5.3
+* Spring Security
+* JPA
+* Hibernate
+* Lombok
--- Server/src/main/java/com/ahttys/server/service/user/AuthServiceImpl.java
+++ Server/src/main/java/com/ahttys/server/service/user/AuthServiceImpl.java
@@ -4,6 +4,7 @@ import com.ahttys.server.domain.user.UserRepository;
 import com.ahttys.server.dto.auth.Auth;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Service
@@ -11,16 +12,19 @@ public class AuthServiceImpl implements AuthService {
     private UserRepository userRepository;
 
     @Override
+    @Transactional
     public Long createUser(Auth.CreateUser userDto) {
         return userRepository.save(userDto.toEntity()).getId();
     }
 
     @Override
+    @Transactional
     public boolean isValidEmail(String email) {
         return userRepository.findByEmail(email).isEmpty();
     }
 
     @Override
+    @Transactional
     public boolean isValidName(String name) {
         return userRepository.findByName(name).isEmpty();
     }
