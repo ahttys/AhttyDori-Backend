5c203fb129f7b03abda91e03efd5b5f6f177f56b
jrhong95 <jrhong95@gmail.com>
2021-08-05 19:07:21 +0900

    Add travisCI

--- Server/src/main/java/com/ahttys/server/service/user/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.ahttys.server.service.user;
-
-import com.ahttys.server.dto.auth.Auth;
-
-public interface AuthService {
-    public Long createUser(Auth.CreateUser userDto);
-    public boolean isValidEmail(String email);
-    public  boolean isValidName(String name);
-}
