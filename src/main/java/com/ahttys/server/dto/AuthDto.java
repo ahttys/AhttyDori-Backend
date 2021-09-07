2a772498ab9b051afd4a8037da01c5e795ae3262
jrhong95 <jrhong95@gmail.com>
2021-09-07 23:45:12 +0900

    Fix jackson error

--- src/main/java/com/ahttys/server/dto/AuthDto.java
+++ src/main/java/com/ahttys/server/dto/AuthDto.java
@@ -1,10 +1,7 @@
 package com.ahttys.server.dto;
 
 import com.ahttys.server.domain.user.User;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.validation.constraints.NotNull;
@@ -14,6 +11,7 @@ public class AuthDto {
     @Getter
     @Setter
     @Builder
+    @NoArgsConstructor
     @AllArgsConstructor
     public static class CreateUser {
         @NotNull
@@ -34,6 +32,7 @@ public class AuthDto {
     @Getter
     @Setter
     @Builder
+    @NoArgsConstructor
     @AllArgsConstructor
     public static class UserResponse{
         @NotNull
@@ -45,6 +44,7 @@ public class AuthDto {
     @Getter
     @Setter
     @Builder
+    @NoArgsConstructor
     @AllArgsConstructor
     public static class Login {
         @NotNull
